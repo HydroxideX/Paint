58ed49487238421266fc7fba7e0574e1a5a2d8f0
HydroxideX <YahiaElsaadawy@gmail.com>
2019-10-30 15:42:29 +0200

    shelt El name heeh

--- src/main/java/eg/edu/alexu/csd/oop/draw/ID71_ID75/draw/Paint.java
+++ src/main/java/eg/edu/alexu/csd/oop/draw/ID71_ID75/draw/Paint.java
@@ -21,67 +21,7 @@ public class Paint extends Application implements DrawingEngine{
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
         Canvas canvas = new Canvas(650,700);
-
-        FXGraphics2D name = new FXGraphics2D(canvas.getGraphicsContext2D());
-        Circle c= new Circle();
-        line l=new line();
-        l.setPosition(new Point(100,100));
-        Map<String,Double> m;
-        m=l.getProperties();
-        m.put("x2", 500d);
-        m.put("y2", 500d);
-        l.setProperties(m);
-        l.draw(name);
-        c.setPosition(new Point(200,200));
-        c.setColor(Color.black);
-        c.setFillColor(Color.red);
-
-        m=c.getProperties();
-        m.put("radius", 300d);
-        c.setProperties(m);
-       // c.draw(name);
-        Circle x= (Circle) c.clone();
-        x.draw(name);
-
-        Rectangle r=new Rectangle();
-        r.setPosition(new Point(50,50));
-        m=r.getProperties();
-        m.put("width", 100d);
-        m.put("height", 200d);
-        r.setProperties(m);
-        r.setColor(Color.yellow);
-        r.setFillColor(Color.green);
-        r.draw(name);
-
-        Square s=new Square();
-        s.setPosition(new Point(250,250));
-        m=s.getProperties();
-        m.put("length", 100d);
-        s.setProperties(m);
-        s.setColor(Color.black);
-        s.setFillColor(Color.blue);
-        s.draw(name);
-
-        Ellipse e=new Ellipse();
-        e.setPosition(new Point(400,400));
-        m=e.getProperties();
-        m.put("width",200d);
-        m.put("height",300d);
-        e.setProperties(m);
-        e.setColor(Color.blue);
-        e.setFillColor(Color.yellow);
-        e.draw(name);
-
-        Triangle t=new Triangle();
-        t.setPosition(new Point(1,500));
-        m=t.getProperties();
-        m.put("x2",500d);
-        m.put("x3",500d);
-        m.put("y2",200d);
-        m.put("y3",10d);
-        t.setProperties(m);
-        t.draw(name);
-
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root,Region.USE_PREF_SIZE,Region.USE_PREF_SIZE));
         primaryStage.show();
