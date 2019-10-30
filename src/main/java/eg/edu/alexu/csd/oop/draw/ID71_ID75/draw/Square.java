bda9e27a5571004dd121307bbe50927074301556
HydroxideX <YahiaElsaadawy@gmail.com>
2019-10-31 00:13:48 +0200

    fkrt sh3rawi

--- src/main/java/eg/edu/alexu/csd/oop/draw/ID71_ID75/draw/Square.java
+++ src/main/java/eg/edu/alexu/csd/oop/draw/ID71_ID75/draw/Square.java
@@ -43,15 +43,14 @@ public class Square implements Shape  {
 
     public void draw(Graphics canvas) {
         Point p1 = new Point(getProperties().get("x2").intValue(),getProperties().get("y2").intValue());
-        Point p2 = new Point();
-        p2.x = position.x;
-        p2.y = position.y;
-        Correct(p2,p1);
-        Double j = max(Double.valueOf(Math.abs(p1.x-p2.x)),Double.valueOf(Math.abs(p1.y-p2.y)));
+        Point p3 = Correct(position,p1);
+        Double l = Double.valueOf(Math.abs(p1.x-position.x));
+        Double w = Double.valueOf(Math.abs(p1.y-position.y));
+        Double mn = Double.valueOf(min(l.intValue(),w.intValue()));
         canvas.setColor(getFillColor());
-        canvas.fillRect(p1.x,p1.y,j.intValue(),j.intValue());
+        canvas.fillRect(p3.x,p3.y,mn.intValue(),mn.intValue());
         canvas.setColor(getColor());
-        canvas.drawRect(p1.x,p1.y,j.intValue(),j.intValue());
+        canvas.drawRect(p3.x,p3.y,mn.intValue(),mn.intValue());
     }
 
     public Object clone() throws CloneNotSupportedException {
@@ -63,31 +62,14 @@ public class Square implements Shape  {
         return c;
     }
 
-    void Correct(Point p1,Point p2){
+    Point Correct(Point p1,Point p2){
         Point p3 = new Point();
-        if(p1.x <= p2.x && p1.y <= p2.y) return;
-        else if(p1.x >= p2.x && p2.y <= p1.y) {
-            p3.x = p1.x;
-            p3.y = p1.y;
-            p1.x = p2.x;
-            p1.y = p2.y;
-            p2.x = p3.x;
-            p2.y = p3.y;
-        }
-        else if(p1.x >= p2.x && p2.y >= p1.y) {
-            p3.x = p1.x;
-            p1.x = p2.x;
-            p2.x = p3.x;
-        }
-        else if(p1.x <= p2.x && p2.y <= p1.y) {
-            p3.y = p1.y;
-            p1.y = p2.y;
-            p2.y = p3.y;
-        }
+        p3.x = min(p1.x,p2.x);
+        p3.y = min(p1.y,p2.y);
+        return p3;
     }
-
-    Double max(Double v,Double r){
-        if(v >= r) return v;
-        return r;
+    int min(int a,int b){
+        if(a<b) return a;
+        return b;
     }
 }
